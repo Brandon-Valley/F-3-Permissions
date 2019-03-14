dfb2ee0d0aeddd260415201eeb96ad35b678586d
Brandon Valley <bavvh8@mst.edu>
2019-03-13 21:26:43 -0500

    added to parse

--- main.cpp
+++ main.cpp
@@ -21,26 +21,40 @@ Dir * parse(const string command, Dir * cur_dir, Membership_Database md)
 	else if (cv.size() == 1 and   cv[0] == "exit")  { exit(0); } //command to stop loop
 	else if (cv.size() == 1 and   cv[0] == "pwd")   { cout << dp_cur_dir->pwd() << endl; }
 	else if (cv.size() == 1 and   cv[0] == "ls")    { dp_cur_dir->ls(md); }
+	else if (cv.size() == 1 and   cv[0] == "groups")    { md.groups(); }
+
 
 	else if (cv.size() == 1 and   cv[0].rfind("./", 0) == 0)    { dp_cur_dir->dot_slash(cv[0].substr(2), md); } // if ./ at beginning of str, do dot_slash on str[2:-1]
 
 
-	else if (cv.size() == 2 and   cv[0] == "cd")     { return cur_dir->cd   (cv[1]); }
-	else if (cv.size() == 2 and   cv[0] == "mkdir")  {        cur_dir->mkdir(cv[1], md); }
-	else if (cv.size() == 2 and   cv[0] == "rmdir")  {        cur_dir->rmdir(cv[1], md); }
-	else if (cv.size() == 2 and   cv[0] == "touch")  {        cur_dir->touch(cv[1], md); }
-	else if (cv.size() == 2 and   cv[0] == "rm")     {        cur_dir->rm   (cv[1], md); }
+	else if (cv.size() == 2 and   cv[0] == "cd")       { return cur_dir->cd   (cv[1]); }
+	else if (cv.size() == 2 and   cv[0] == "mkdir")    {        cur_dir->mkdir(cv[1], md); }
+	else if (cv.size() == 2 and   cv[0] == "rmdir")    {        cur_dir->rmdir(cv[1], md); }
+	else if (cv.size() == 2 and   cv[0] == "touch")    {        cur_dir->touch(cv[1], md); }
+	else if (cv.size() == 2 and   cv[0] == "rm")       {        cur_dir->rm   (cv[1], md); }
+	else if (cv.size() == 2 and   cv[0] == "groupadd") {        md.groupadd   (cv[1]    ); }
+	else if (cv.size() == 2 and   cv[0] == "useradd")  {        md.useradd    (cv[1]    ); }
+	else if (cv.size() == 2 and   cv[0] == "userdel")  {        md.userdel    (cv[1]    ); }
+	else if (cv.size() == 2 and   cv[0] == "switchto") {        md.switchto   (cv[1]    ); }
 
 	else if (cv.size() == 2 and   cv[0] == "ls" and cv[1] == "-l") { cur_dir->ls_l(md); }
 
-	else if (cv.size() == 3 and   cv[0] == "chmod")                { cur_dir->chmod(cv[2], cv[1]); }
+
+	else if (cv.size() == 3 and   cv[0] == "chmod")           { cur_dir->chmod(cv[2], cv[1]    ); }
+	else if (cv.size() == 3 and   cv[0] == "chown")           { cur_dir->chown(cv[1], cv[2], md); }
+	else if (cv.size() == 3 and   cv[0] == "chgrp")           { cur_dir->chgrp(cv[1], cv[2], md); }
+
+
+	else if (cv.size() == 4 and   cv[0] == "userdel" and cv[1] == "-G")  { md.userdel_G(cv[2], cv[3]); }
+	else if (cv.size() == 4 and   cv[0] == "useradd" and cv[1] == "-G")  { md.useradd_G(cv[2], cv[3]); }
+
+	else if (cv.size() == 4 and   cv[0] == "usermod" and cv[1] == "-a" and cv[2] == "-G")  { md.usermod_a_G(cv[3]); }
 
 
 	else { throw "No command '" + command + "' found"; }
 
 	return cur_dir;
 }
-
 
 
 
